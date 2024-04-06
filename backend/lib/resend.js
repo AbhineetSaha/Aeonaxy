65ca6a7b524eed9d2e24a822191ef952507176a6
Abhineet Saha <76945162+AbhineetSaha@users.noreply.github.com>
2024-04-06 19:54:34 +0530

    Changed Backend:

--- backend/lib/resend.js
+++
@@ -1,5 +0,0 @@
-const {Resend} = require('resend');
-
-const resend = new Resend('re_SYMrT5Ro_Ab1BSDrXhJGvoyiu8bPSkZV8');
-
-module.exports = { resend };
--- backend/lib/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const supabase = createClient(
-    "https://tgbryxeajqjngiylypgg.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnYnJ5eGVhanFqbmdpeWx5cGdnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjE2NDU5OSwiZXhwIjoyMDI3NzQwNTk5fQ.M8VNpQHJcL3MGjyCsMMcwsdu7ukSTod2c0SadXM2cd8"
-);
-
-
-module.exports = { supabase };
